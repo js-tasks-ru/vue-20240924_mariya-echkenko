db481df8d4f57703ea5c99778d9f4a75ee844328
MariaEchkenko <mashasly@mail.ru>
2024-10-04 11:49:38 +0300

    Create App

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+  setup () {
+    function formatAsLocalDate() {
+      return new Date().toLocaleString(navigator.language, {
+        dateStyle: 'long'
+      })
+    }
+
+    return {
+      formatAsLocalDate,
+    }
+  },
+  template: `<div>Сегодня {{formatAsLocalDate()}}</div>`
+})
+
+const app = createApp(App);
+const vm = app.mount('#app');
